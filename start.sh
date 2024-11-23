d6011d8fa5703937774653f14a891a4d7bc207d5
david reilly <davidreilly02@gmail.com>
2024-11-23 15:10:41 +0000

    deleted and cleaned up repo

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Set PATH for Chrome, needed for Selenium
-export PATH="/opt/render/project/src/.render/chrome/opt/google/chrome:$PATH"
-
-# Start the Flask application using Gunicorn
-gunicorn main:app --timeout 120
